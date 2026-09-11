c061ef9ac019bb73b0b696c579473d9d8d3baf4c
PROGRAMAR NIMAH <208431948+Nimsara0000@users.noreply.github.com>
2026-09-11 16:55:10 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,7 @@ define('DB_PASS', getenv('DB_PASS') ?: '');
 define('DB_PORT', getenv('DB_PORT') ?: '3306');
 
 define('SITE_NAME', 'Nima SMM');
-define('SITE_URL', getenv('SITE_URL') ?: 'http://localhost');
+define('SITE_URL', getenv('SITE_URL') ?: 'https://nima-smm.onrender.com');
 define('PROVIDER_API_URL', 'https://smmpakpanel.com/api/v2');
 define('PROVIDER_API_KEY', '924d90d325b79930a235cc83af4311fe');
 
